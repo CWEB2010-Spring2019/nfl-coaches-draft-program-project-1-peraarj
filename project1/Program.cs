f99073869d3cfd5c6e45f15389f5f16088d173b9
Aaron Perry <peraarj@dunwoody.edu>
2019-02-05 11:59:11 -0600

    Adding player class

--- project1/Program.cs
+++ project1/Program.cs
@@ -1,18 +1,35 @@
 ﻿using System;
+using static System.Console;
+
 
 namespace project1
 {
     class Program
     {
 
-	// Creation of global variables
+		// Creation of global variables
 
-	// Classes and methods
+		// Classes and methods
+
+		public class Player {
+			public string pName { get; set; }
+			public string pPosition { get; set; }
+			public string pInstitution { get; set; }
+			public int pSalarie { get; set; }
+		}
+
 
         static void Main(string[] args) // Start of main method
         {
+
+			Player player01 = new Player();
+			player01.pName = "Freddy";
+			int draftBudget = 95000000;
+			int draftSpent = 0;
+			int playersPicked = 0;
             Console.WriteLine("Hello World!");
-			Console.WriteLine("This is a test push to remote rep");
+			WriteLine(player01.pName);
+			
 
 		} // End of main method
     }
